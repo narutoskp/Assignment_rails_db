406b51c512ff845e62a20d7e2f6e541335531e9d
narutoskp <dragonskp@gmail.com>
2016-01-26 01:12:46 -0800

    3

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,12 +13,30 @@ class RoomsController < ApplicationController
 		@room = Room.new		
 	end
 
+	def edit
+  		@room = Room.find(params[:id])
+	end
+
 	def create
   		@room = Room.new(room_params)
  
   		@room.save
   		redirect_to @room
 	end
+
+	def update
+		@room = Room.find(params[:id])
+ 
+  		@room.update(room_params)
+    	redirect_to @room
+	end
+
+	def destroy
+    	@room = Room.find(params[:id])
+    	@room.destroy
+ 
+    	redirect_to rooms_path
+  	end
  
 	private
   		def room_params
